90faa68d4d67b7b7a59f05ab79ad539d36748254
Henry Ireland <henryireland95@gmail.com>
2023-11-14 10:43:44 +0000

    makingapp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+flats = [
+  { name: 'Naughty Gaff',
+    address: '25 MaryLand Gardens London W9 1DT',
+    description: 'Naughty Basement',
+    price_per_night: 35,
+    number_of_guests: 2 },
+  { name: 'Peng Gaff',
+    address: '25 MaryLand Gardens London W9 1DT',
+    description: 'Naughty Basement',
+    price_per_night: 35,
+    number_of_guests: 2 },
+  { name: 'Best Vibes',
+    address: '85 Highbury vibes 1DT',
+    description: 'Creepy Basement',
+    price_per_night: 24,
+    number_of_guests: 2 }
+]
+
+flats.each do |flat|
+  Flat.create!(flat)
+end
